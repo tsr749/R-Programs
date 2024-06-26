f96139424708b4c6ab89069bcacae461e3c9b6af
tsr749 <164722232+tsr749@users.noreply.github.com>
2024-06-26 13:57:24 +0530

    Delete R Practical Tushar C44.R

--- R Practical Tushar C44.R
+++
@@ -1,9 +0,0 @@
-v1 <- c(0, 3)
-v2 <- c(1, 2)
-
-cat ("Addition of vectors :", v1 + v2, "\n")
-cat ("Subtraction of vectors :", v1 - v2, "\n")
-cat ("Multiplication of vectors :", v1 * v2, "\n")
-cat ("Division of vectors :", v1 / v2, "\n")
-cat ("Modulo of vectors :", v1 %% v2, "\n")
-cat ("Power operator :", v1 ^ v2)
